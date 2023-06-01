ad8e7fb5c5779d929e4971867eb06d192c44a447
t-akira012 <aki003xyz@gmail.com>
2023-06-02 08:21:35 +0900

    [MOD] kickstart.nvim のインストールにコメントを追記

--- scripts/kickstart.sh
+++ scripts/kickstart.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-
+set -eu
 if which ghq >>/dev/null 2>&1 ;then
+  echo === install kickstart nvim
   ghq get -p t-akira012/kickstart.nvim
   ln -si $(ghq root)/github.com/t-akira012/kickstart.nvim $HOME/.config/nvim
+else
+  echo "require ghq"
 fi
