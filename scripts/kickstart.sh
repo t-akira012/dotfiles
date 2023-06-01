93be4d06f874b1fb38b9fb0277c828a8c156483c
t-akira012 <aki003xyz@gmail.com>
2023-06-02 01:46:37 +0900

    [MOD] kickstar.nvim の install 時に標準出力されるコメントを修正

--- scripts/kickstart.sh
+++ scripts/kickstart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if which ghq;then
+if which ghq >>/dev/null 2>&1 ;then
   ghq get -p t-akira012/kickstart.nvim
   ln -si $(ghq root)/github.com/t-akira012/kickstart.nvim $HOME/.config/nvim
 fi
