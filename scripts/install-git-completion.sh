9155c6bde010e6bc97f7756babca42d307d4cc24
t-akira012 <aki003xyz@gmail.com>
2023-06-02 01:34:49 +0900

    [MOD] wget の -p オプションを使わない

--- scripts/install-git-completion.sh
+++ scripts/install-git-completion.sh
@@ -5,8 +5,8 @@ export LOCAL_BIN="$HOME/.local/bin"
 [[ ! -d "$LOCAL_BIN" ]] && mkdir -p $TARGET
 
 cd $LOCAL_BIN
-wget -p $LOCAL_BIN https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-wget -p $LOCAL_BIN https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
 chmod +x git-prompt.sh
 chmod +x git-completion.bash
