408fed100f680b493433fa3c1a225d3e5e50b5ea
t-akira012 <aki003xyz@gmail.com>
2023-07-06 19:06:44 +0900

    tmuxのレイアウトセット用コマンドを追加

--- root/bin/tmux-set.sh
+++ root/bin/tmux-set.sh
@@ -1,8 +1,16 @@
-# tate
-tmux split-window -v -c '#{pane_current_path}' -p 85
-# yoko
-tmux split-window -h -c '#{pane_current_path}' -p 80
-tmux split-window -h -c '#{pane_current_path}' -p 20
+if [[ $(tmux display -p "#{window_panes}" ) -eq 1 ]];then
+    # tate
+    tmux split-window -v -c '#{pane_current_path}' -p 85
+    # yoko
+    tmux split-window -h -c '#{pane_current_path}' -p 80
+    tmux split-window -h -c '#{pane_current_path}' -p 20
 
-# set
-tmux select-pane -t 3
+    # command
+    tmux send-key -t 4 "doc-today" C-m
+
+    # select
+    tmux select-pane -t 3
+
+else
+    echo already tmux has some panes.
+fi
