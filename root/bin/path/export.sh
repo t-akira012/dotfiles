d98a06e2c5fdae34670ebc15ec15fc5f56684647
t-akira012 <aki003xyz@gmail.com>
2023-07-11 10:58:09 +0900

    change fzf colorscheme

--- root/bin/path/export.sh
+++ root/bin/path/export.sh
@@ -71,25 +71,19 @@ fi
 # fzf
 # https://github.com/junegunn/fzf/wiki/Color-schemes
 _gen_fzf_default_opts() {
-  local base03="234"
-  local base02="235"
-  local base01="240"
-  local base00="241"
-  local base0="244"
-  local base1="245"
-  local base2="254"
-  local base3="230"
-  local yellow="136"
-  local orange="166"
-  local red="160"
-  local magenta="125"
-  local violet="61"
-  local blue="33"
-  local cyan="37"
-  local green="64"
-
-  local FZF_DARK="--color fg:-1,bg:-1,hl:$blue,fg+:$base2,bg+:$base02,hl+:$blue --color info:$yellow,prompt:$yellow,pointer:$base3,marker:$base3,spinner:$yellow"
-  local FZF_LIGHT="--color fg:-1,bg:-1,hl:$blue,fg+:$base02,bg+:$base2,hl+:$blue --color info:$yellow,prompt:$yellow,pointer:$base03,marker:$base03,spinner:$yellow"
+  # https://github.com/jez/dotfiles/blob/master/util/fzf.zsh#L47C1-L58C4
+  # Solarized Dark color scheme for fzf
+  export FZF_DARK="
+    --color fg:-1,bg:-1,hl:$blue,fg+:$base2,bg+:-1,hl+:$blue
+    --color info:$yellow,prompt:$yellow,pointer:$base3,marker:$base3,spinner:$yellow
+    --no-separator
+  "
+  # Solarized Light color scheme for fzf
+  export FZF_LIGHT="
+    --color fg:-1,bg:-1,hl:$blue,fg+:$base02,bg+:-1,hl+:$blue
+    --color info:$yellow,prompt:$yellow,pointer:$base03,marker:$base03,spinner:$yellow
+    --no-separator
+  "
   if [[ "$TERM_COLOR_MODE" == "LIGHT" ]]; then
     export COLOR_OPTS="$FZF_LIGHT"
   else
