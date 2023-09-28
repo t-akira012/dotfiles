06b059c9f95c67030ea56ea058b219bde35db51f
t-akira012 <aki003xyz@gmail.com>
2023-09-28 22:41:37 +0900

    fix: 🐛 fix curl option typo

--- init.sh
+++ init.sh
@@ -59,8 +59,8 @@ export LOCAL_BIN="$HOME/.local/bin"
 [[ ! -d "$LOCAL_BIN" ]] && mkdir -p $LOCAL_BIN
 
 cd $LOCAL_BIN
-curl -o https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-curl -o https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
 chmod +x git-prompt.sh
 chmod +x git-completion.bash
