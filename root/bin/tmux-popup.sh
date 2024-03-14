9788fdd565bb4e334e63056d292b5edd7f0ead81
t-akira012 <aki003xyz@gmail.com>
2024-03-14 13:07:29 +0900

    refactor: 💡 update tmux conf

--- root/bin/tmux-popup.sh
+++ root/bin/tmux-popup.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# tmux popup windowをトグルする関数
-function tmuxpopup(){
-    # ウィンドウのサイズを設定
-    width='50%'
-    height='100%'
+# 現在のセッション名を取得
+session=$(tmux display-message -p -F "#{session_name}")
 
-    # 現在のセッション名を取得
-    session=$(tmux display-message -p -F "#{session_name}")
+# tmux popup windowをトグルする関数
+max(){
+    # ウィンドウのサイズを設定
+    local width='100%'
+    local height='100%'
+
+    # セッション名に"popup"が含まれるかチェック
+    if [[ $session == *"popup"* ]]; then
+        # "popup"が含まれる場合、クライアントをデタッチ（ポップアップを閉じる）
+        tmux detach-client
+    else
+        # "popup"が含まれない場合、新しいポップアップウィンドウを表示
+        tmux popup -d '#{pane_current_path}' -xC -yC -w$width -h$height -E "tmux attach -t popup || tmux new -s popup"
+    fi
+}
+
+right(){
+    # ウィンドウのサイズを設定
+    local width='50%'
+    local height='100%'
 
     # セッション名に"popup"が含まれるかチェック
     if [[ $session == *"popup"* ]]; then
@@ -19,4 +34,8 @@ function tmuxpopup(){
     fi
 }
 
-tmuxpopup &
+if [[ "$1" == "right" ]];then
+ right &
+elif [[ "$1" == "max" ]];then
+ max &
+fi
