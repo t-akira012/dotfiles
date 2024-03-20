77e903c0562a6992ac20923049c320d97017c55c
t-akira012 <aki003xyz@gmail.com>
2024-03-20 23:08:20 +0900

    refactor: 💡 update

--- scripts/manjaro.sh
+++ scripts/manjaro.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+
+[[ !-e $HOME/.ssh ]] && mkdir $HOME/.ssh
+
 # サスペンド無効化
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
-# 日本語入力
-sudo pacman -Syy
-sudo pacman -S manjaro-asian-input-support-ibus gtk2 fcitx5 fcitx5-configtool fcitx5-mozc fcitx5-im
+# ssh 有効化
+sudo pacman -S openssh
+sudo systemctl restart sshd
+sudo systemctl enable sshd
 
-# ホームディレクトリの英語化
-sudo pacman -S xdg-user-dirs-gtk
-LC_ALL=C xdg-user-dirs-gtk-update
--- scripts/manjaro_ssh.sh
+++ scripts/manjaro_ssh.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-sudo pacman -S openssh
-sudo systemctl enable sshd
+# 日本語入力
+sudo pacman -Syy
+sudo pacman -S manjaro-asian-input-support-ibus gtk2 fcitx5 fcitx5-configtool fcitx5-mozc fcitx5-im
+
+# ホームディレクトリの英語化
+sudo pacman -S xdg-user-dirs-gtk
+LC_ALL=C xdg-user-dirs-gtk-update
