ba4a2495a5fb1937f8a2bba81f3a6c159da5cbb3
t-akira012 <aki003xyz@gmail.com>
2024-10-31 01:26:27 +0900

    update

--- .ex
+++ .ex
@@ -1,4 +1,6 @@
 alias cdoc="cd $HOME/docs/doc"
+export GIST_DIR="$HOME/src/github.com/t-akira012/aki012/gists"
+export MEMO_DIR="$HOME/docs/doc"
 export TERM_COLOR_MODE='DARK'
 export NVIM_COLOR_DARK='vim-material'
 export NVIM_COLOR_LIGHT='vim-material'
