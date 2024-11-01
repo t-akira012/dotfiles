69f537ef576d1233ba6e63e5715dd31f922a0716
t-akira012 <aki003xyz@gmail.com>
2024-11-01 18:06:26 +0900

    update

--- root/.config/xremap/map.rb
+++ root/.config/xremap/map.rb
@@ -1,7 +1,50 @@
 # vim:fenc=utf-8 ff=unix ft=ruby ts=2 sw=2 sts=2
 # https://github.com/hachibeeDI/dotfiles/blob/master/.xremap.conf
 
-window class_not: 'Alacritty' do
+# window class_not: 'Alacritty' do
+#   remap 'C-b', to: 'Left'
+#   remap 'C-f', to: 'Right'
+#   remap 'C-p', to: 'Up'
+#   remap 'C-n', to: 'Down'
+#   remap 'C-a', to: 'Home'
+#   remap 'C-e', to: 'End'
+#
+#   remap 'C-k', to: ['Shift-End', 'Ctrl-C', 'BackSpace']
+#   remap 'C-u', to: ['Shift-Home', 'BackSpace']
+#   remap 'C-y', to: 'Shift-Insert'
+#
+#   remap 'C-h', to: 'BackSpace'
+#   remap 'C-d', to: 'Delete'
+#   remap 'C-m', to: 'Enter'
+#
+#   remap 'C-w', to: 'Ctrl-BackSpace'
+#
+#   %w[a c f l n r v p w t x z y t h].each do |key|
+#     remap "Super-#{key}", to: "C-#{key}"
+#   end
+#   %w[n t r z].each do |key|
+#     remap "Super-Shift-#{key}", to: "C-#{key}"
+#   end
+#
+#   (0..9).each do |key|
+#     remap "Super-#{key}", to: "C-#{key}"
+#   end
+#
+#   remap 'Super-Enter', to: 'M-Enter'
+#   remap 'Super-Equal', to: 'Ctrl-Equal'
+#   remap 'Super-Minus', to: 'Ctrl-Minus'
+#
+#   remap 'Super-Shift-t', to: 'Shift-Ctrl-t'
+# end
+
+
+# 効かないので alacritty.toml 側で制御
+# window class_only: 'Alacritty' do
+#   remap 'Super-v', to: 'C-Shift-v'
+# end
+
+# mac likeにする
+window class_only: 'google-chrome' do
   remap 'C-b', to: 'Left'
   remap 'C-f', to: 'Right'
   remap 'C-p', to: 'Up'
@@ -35,11 +78,6 @@ window class_not: 'Alacritty' do
   remap 'Super-Minus', to: 'Ctrl-Minus'
 
   remap 'Super-Shift-t', to: 'Shift-Ctrl-t'
-end
-
-
-window class_only: 'Alacritty' do
-  remap 'Super-v', to: 'C-Shift-v'
 end
 
 window class_only: 'slack' do
