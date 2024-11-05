e30d083ec0b4c3a23cfd8328afb1534a0a7bc92d
t-akira012 <aki003xyz@gmail.com>
2024-11-05 13:06:42 +0900

    fixtypo

--- root/bin/tmux-toggle-window.sh
+++ root/bin/tmux-toggle-window.sh
@@ -8,7 +8,7 @@ TARGET_WINDOW=9
 if [[ $TARGET_WINDOW -ne $CURRENT_WINDOW ]]; then
 	echo $CURRENT_WINDOW >$HOME/.tmux_toggle_window
 
-	if tmux list-windows -F '#I' | rg 9 >/dev/null &>1; then
+	if tmux list-windows -F '#I' | rg 9 >/dev/null 2>&1; then
 		tmux select-window -t $TARGET_WINDOW
 	else
 		tmux new-window -n "doc" -t 9
@@ -16,9 +16,9 @@ if [[ $TARGET_WINDOW -ne $CURRENT_WINDOW ]]; then
 else
 	LATEST_WINDOW=$(cat $HOME/.tmux_toggle_window)
   # 前回のWindowの存在チェック
-  if tmux list-windows -F "#{window_index}" | rg -e "^${LATEST_WINDOW}$" > /dev/null 2&>1 ;then
+  if tmux list-windows -F "#{window_index}" | rg -e "^${LATEST_WINDOW}$" > /dev/null 2>&1 ;then
     # 前回のWindowに移動
-    tmux select-window -t $LATEST_WINDOW > /dev/null 2&>1
+    tmux select-window -t $LATEST_WINDOW > /dev/null 2>&1
   else
     # 最初のWindowに遷移
     tmux select-window -t $(tmux list-windows -F "#{window_index}" | head -n 1)
