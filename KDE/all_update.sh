8cf9c9075570c6bc07b09c5aa88bf7d1b25b2930
t-akira012 <aki003xyz@gmail.com>
2024-11-14 22:42:14 +0900

    update

--- KDE/all_update.sh
+++ KDE/all_update.sh
@@ -4,4 +4,4 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove
 sudo $HOME/dotfiles/scripts/install_firefox_dev.sh
-$HOME/dotfiles/scripts/_build_neovim_stable.sh
+sudo $HOME/dotfiles/scripts/_build_neovim_stable.sh
