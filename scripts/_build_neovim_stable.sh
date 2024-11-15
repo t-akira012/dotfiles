12957577d07d1c7d4851e272b3dd3fc6e6a75a6d
t-akira012 <aki003xyz@gmail.com>
2024-11-15 22:39:22 +0900

    update

--- scripts/_build_neovim_stable.sh
+++ scripts/_build_neovim_stable.sh
@@ -7,6 +7,7 @@ fi
 set -eux
 
 echo Build
+rm -rf /tmp/neovim-git
 git clone https://github.com/neovim/neovim.git -b stable --depth 1 /tmp/neovim-git
 cd /tmp/neovim-git
 make CMAKE_BUILD_TYPE=Release \
---
+++ scripts/install_samba.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+
+if type samba >/dev/null 2>&1;then
+  sudo systemctl status smbd.service
+  exit 0
+fi
+
+USER=$(whoami)
+sudo apt update -y
+sudo apt install samba
+
+sudo systemctl enable smbd
+sudo systemctl enable nmbd
+sudo systemctl start smbd
+sudo systemctl start nmbd
+
+
+cat <<EOF | sudo tee -a /etc/samba/smb.conf
+
+[storage]
+comment = Storage
+path = /mnt/storage
+writable = yes
+guest ok = No
+guest only = No
+force user = aki
+EOF
+
+sudo smbpasswd -a $USER
+sudo smbpasswd -e $USER
+
+sudo systemctl enable smbd.service
+sudo systemctl start smbd.service
