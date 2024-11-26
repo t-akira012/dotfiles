314c265ee057561e553bac8c9f57e9a705d78395
t-akira012 <aki003xyz@gmail.com>
2024-11-26 11:50:56 +0900

    update

--- asdf/_install_tools.sh
+++ asdf/_install_tools.sh
@@ -10,19 +10,16 @@ go install golang.org/x/tools/gopls@latest
 go install github.com/google/yamlfmt/cmd/yamlfmt@latest
 go install github.com/x-motemen/ghq@latest
 
-[[ ! -d $HOME/.local/repos ]] && mkdir $HOME/.local/repos
-
-go install github.com/x-motemen/ghq@latest
-
-git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.local/repos/fzf \
-    && cp $HOME/.local/repos/fzf/bin/fzf $HOME/.local/bin/fzf
 
 # fzf
-[[ ! -d $HOME/.local/bin ]] && mkdir -p $HOME/.local/bin/
+[[ ! -d $HOME/.local/ ]] && mkdir $HOME/.local/
+[[ ! -d $HOME/.local/repos ]] && mkdir $HOME/.local/repos
+[[ ! -d $HOME/.local/bin ]] && mkdir $HOME/.local/bin
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.local/repos/fzf
+bash -c $HOME/.local/repos/fzf/install
+cp $HOME/.local/repos/fzf/bin/fzf $HOME/.local/bin/fzf
 curl -s https://raw.githubusercontent.com/junegunn/fzf/master/bin/fzf-preview.sh -o $HOME/.local/bin/fzf-preview.sh
 chmod +x $HOME/.local/bin/fzf-preview.sh
-
-
 
 #########################################################
 echo Install rust tools
---
+++ git-hooks/install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# pre-push
+
+mkdir $HOME/.config/git-hooks
+cat EOF<< > $HOME/.config/git-hooks/pre-push
+#!/bin/bash
+
+_exec_localhook(){
+  GIT_ROOT=$(git rev-parse --show-superproject-working-tree --show-toplevel | head -1)
+  HOOK_NAME=$(basename $0)
+  LOCAL_HOOK="${GIT_ROOT}/.git/hooks/${HOOK_NAME}"
+  if [ -e $LOCAL_HOOK ]; then
+    source $LOCAL_HOOK
+  fi
+}
+
+REMOTE_URL=$(git config --get remote.origin.url)
+ALLOW_PUSH_ORGS=(xxx) # 要変更
+
+echo "========================================================="
+for org in "${ALLOW_PUSH_ORGS[@]}"; do
+  if [[ $REMOTE_URL == "https://github.com/${org}/"* ]];then
+    echo "[Allow push: ${org}"]
+    echo "Remote url is $REMOTE_URL"
+    echo "========================================================="
+    _exec_localhook
+    exit 0
+  fi
+done
+
+echo "[Deny push]"
+echo "Remote url is $REMOTE_URL"
+exit 1
+echo "========================================================="
+EOF
+
+chmod +x $HOME/.config/git-hooks/pre-push
+git config --global core.hooksPath .config/git-hooks
