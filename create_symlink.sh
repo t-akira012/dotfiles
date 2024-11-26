d1fd0686a6b75d6969bb1753423fb1789eb3fcc1
t-akira012 <aki003xyz@gmail.com>
2024-11-26 11:09:48 +0900

    update

--- create_symlink.sh
+++ create_symlink.sh
@@ -4,30 +4,41 @@ echo Create symbolic links
 #########################################################
 
 BASEPATH=$HOME/dotfiles/root
-[[ ! -d $HOME/.config ]] && mkdir $HOME/.config
 
+for_all(){
+  [[ ! -d $HOME/.config ]] && mkdir $HOME/.config
 
-[[ -e $HOME/.bashrc ]] && mv "$HOME/.bashrc" "$HOME/.bashrc.bak.$(date +"%Y%m%d_%H%M")"
-ln -si $BASEPATH/.bashrc $HOME/.bashrc
+  [[ -e $HOME/.bashrc ]] && mv "$HOME/.bashrc" "$HOME/.bashrc.bak.$(date +"%Y%m%d_%H%M")"
+  symlink $BASEPATH/.bashrc $HOME/.bashrc
 
-ln -si $BASEPATH/bin $HOME/bin
-ln -si $BASEPATH/.tmux.conf $HOME/.tmux.conf
-ln -si $BASEPATH/.config/git $HOME/.config/git
-ln -si $BASEPATH/.config/starship.toml $HOME/.config/starship.toml
-ln -si $BASEPATH/.config/alacritty/ $HOME/.config/alacritty
+  [[ -e $HOME/.zshrc ]] && mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +"%Y%m%d_%H%M")"
+  symlink $BASEPATH/.config/zsh $HOME/.config/zsh
+  symlink $BASEPATH/.config/zsh/.zshrc $HOME/.zshrc
+
+  symlink $BASEPATH/bin $HOME/bin
+  symlink $BASEPATH/.tmux.conf $HOME/.tmux.conf
+  symlink $BASEPATH/.config/git $HOME/.config/git
+  symlink $BASEPATH/.config/starship.toml $HOME/.config/starship.toml
+  symlink $BASEPATH/.config/alacritty/ $HOME/.config/alacritty
+}
+
+symlink(){
+  if [[ ! -e $2 ]];then
+    ln -si $1 $2
+  else
+    echo "$2 is already exist"
+  fi
+}
 
 for_darwin(){
-  ln -si $BASEPATH/root/.config/amethyst $HOME/.config/amethyst
+  :
 }
 
 for_linux(){
-  ln -si $BASEPATH/root/.config/xremap $HOME/.config/xremap
+  symlink $BASEPATH/root/.config/xremap $HOME/.config/xremap
 }
 
-if [[ $(uname) == "Darwin"]];then
-  for_darwin
-fi
+for_all
 
-if [[ $(uname) == "Linux"]];then
-  for_linux
-fi
+[ "$(uname)" == "Darwin" ] && for_linux
+[ "$(uname)" == "Linux" ] && for_linux
--- scripts/forzsh.sh
+++ scripts/forzsh.sh
@@ -2,37 +2,35 @@
 set -eu
 cd $(dirname $0)
 
-[[ -e $HOME/.zshrc ]] && mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +"%Y%m%d_%H%M")"
+clone_zsh_function(){
+  cd $HOME/.config/zsh
 
-echo Create symbolic links
-BASEPATH=$HOME/dotfiles/root
-[[ ! -d $HOME/.config ]] && mkdir $HOME/.config
-ln -si $BASEPATH/.config/zsh $HOME/.config/zsh
-ln -si $BASEPATH/.config/zsh/.zshrc $HOME/.zshrc
+  [[ ! -d repos ]] && mkdir repos
+  [[ ! -d zfunc ]] && mkdir zfunc
 
-echo Clone zsh function
+  cd $HOME/.config/zsh/repos
 
-[[ ! -d ~/.config/zsh ]] && \
-  echo "zsh config directory not found" && \
-  exit 1
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting --depth 1
+  git clone https://github.com/zsh-users/zsh-autosuggestions --depth 1
+  git clone https://github.com/zsh-users/zsh-completions --depth 1
+  git clone https://github.com/intelfx/pure --depth 1
+  git clone https://github.com/ohmyzsh/ohmyzsh --depth 1
+  git clone https://github.com/Tarrasch/zsh-bd --depth 1
+}
 
-cd $HOME/.config/zsh
+pure_install(){
+  # pure install
+  cd $HOME/.config/zsh/repos/pure
+  ln -s "$PWD/pure.zsh" $HOME/.config/zsh/zfunc/prompt_pure_setup
+  ln -s "$PWD/async.zsh" $HOME/.config/zsh/zfunc/async
+}
 
-[[ ! -d repos ]] && mkdir repos
-[[ ! -d zfunc ]] && mkdir zfunc
 
-cd $HOME/.config/zsh/repos
+if [ -L $HOME/.config/zsh ]; then
+  clone_zsh_function
+  # pure_install
+  chsh -s $(which zsh)
+else
+  echo ".config/zshがsymlinkではない"
+fi
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting --depth 1
-git clone https://github.com/zsh-users/zsh-autosuggestions --depth 1
-git clone https://github.com/zsh-users/zsh-completions --depth 1
-git clone https://github.com/intelfx/pure --depth 1
-git clone https://github.com/ohmyzsh/ohmyzsh --depth 1
-git clone https://github.com/Tarrasch/zsh-bd --depth 1
-
-# pure install
-cd $HOME/.config/zsh/repos/pure
-ln -s "$PWD/pure.zsh" $HOME/.config/zsh/zfunc/prompt_pure_setup
-ln -s "$PWD/async.zsh" $HOME/.config/zsh/zfunc/async
-
-chsh -s $(which zsh)
