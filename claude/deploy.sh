1fb17a073a0c8092554be1095737ffc8353aacc1
t-akira012 <aki003xyz@gmail.com>
2025-06-17 21:51:08 +0900

    upda

--- claude/deploy.sh
+++ claude/deploy.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash -eux
 cd $(dirname $0)
 
-files=(CLAUDE.md CLAUDE.ref.md settings.json commands)
-
-for item in "${files[@]}"; do
-    ln -si "$PWD/$item" $HOME/.claude/$item
-done
+ln -si "$PWD" $HOME/.claude
