d7d6196fb0d4e44e163c6eb5dc03521067ca9857
t-akira012 <aki003xyz@gmail.com>
2025-06-23 18:20:09 +0900

    update

--- root/bin/tmux-dev.sh
+++ root/bin/tmux-dev.sh
@@ -8,19 +8,11 @@ tmux has-session -t $SESSION_NAME 2>/dev/null
 if [ $? \!= 0 ]; then
     # セッションが存在しない場合、新規作成
     
-    # ウィンドウ1: dev-editor
-    tmux new-session -d -s $SESSION_NAME -n "dev-editor"
-    
-    # ウィンドウ2: dev-document
-    tmux new-window -t $SESSION_NAME:2 -n "dev-document"
-    
-    # ウィンドウ7: other
+    tmux new-session -d -s $SESSION_NAME -n "finder"
+    tmux new-window -t $SESSION_NAME:2 -n "dev-editor"
+    tmux new-window -t $SESSION_NAME:3 -n "dev-document"
     tmux new-window -t $SESSION_NAME:7 -n "other"
-    
-    # ウィンドウ8: environment
     tmux new-window -t $SESSION_NAME:8 -n "environment"
-    
-    # ウィンドウ9: doc
     tmux new-window -t $SESSION_NAME:9 -n "doc"
     
     # 最初のウィンドウを選択
