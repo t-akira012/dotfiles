2ea12f12117452efe7a7ca41c8cacd6cf96c21c3
t-akira012 <aki003xyz@gmail.com>
2025-06-23 18:42:26 +0900

    update

--- root/bin/tmux-toggle.sh
+++ root/bin/tmux-toggle.sh
@@ -5,7 +5,7 @@ CURRENT_SESSION=$(tmux display-message -p -F "#{session_name}")
 if [[ "$CURRENT_SESSION" == "term_on_vim"* ]] ; then
 	$HOME/bin/tmux-popup.sh
 elif [[ "$CURRENT_SESSION" == "claude-code"* ]];then
-	$HOME/bin/tmux-popup2.sh
+	exit 0 ;$HOME/bin/tmux-popup2.sh
 else
 	$HOME/bin/tmux-toggle-window.sh
 fi
