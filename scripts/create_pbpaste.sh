b6f54f19f388fa6f1407f717bb6b0e0244d88d96
t-akira012 <aki003xyz@gmail.com>
2025-07-04 23:54:50 +0900

    update

--- scripts/create_pbpaste.sh
+++ scripts/create_pbpaste.sh
@@ -4,7 +4,7 @@ read -p "ユーザー名は？" 'USER_NAME'
 read -p "SSHファイル名は？" 'SSH_NAME'
 cat | sudo tee /usr/local/bin/pbpaste <<EOF
 #!/usr/bin/env bash
-MAC_IP=\$(echo \$SSH_CLIENT | cut -d' ' -f1)
+MAC_IP=\$(echo \$SSH_CONNECTION | cut -d' ' -f1)
 ssh -p ${PORT} -i ~/.ssh/${SSH_NAME} ${USER_NAME}@\${MAC_IP} pbpaste
 EOF
 
