8720bcf498bc338fd3279e480e18e17db620d09c
t-akira012 <aki003xyz@gmail.com>
2025-10-15 12:11:09 +0900

    update

--- root/bin/tmux-dev.sh
+++ root/bin/tmux-dev.sh
@@ -11,8 +11,6 @@ if [ $? \!= 0 ]; then
     tmux new-session -d -s $SESSION_NAME -n "f"
     tmux new-window -t $SESSION_NAME:2 -n "dev"
     tmux new-window -t $SESSION_NAME:3 -n "doc"
-    tmux new-window -t $SESSION_NAME:6 -n "other"
-    tmux new-window -t $SESSION_NAME:7 -n "vibe"
     tmux new-window -t $SESSION_NAME:8 -n "env"
     tmux new-window -t $SESSION_NAME:9 -n "doc" -c $HOME/docs/doc/
     
