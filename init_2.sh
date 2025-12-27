71f440762b8cab4a3ce9c400707d7706aa7ea7dc
t-akira012 <t-akira012@t-akira012noMac-mini.local>
2025-12-27 13:53:26 +0900

    update

--- init_2.sh
+++ init_2.sh
@@ -7,7 +7,10 @@ cp ./.ex $HOME/.ex
 ./init_2_create_symlink.sh
 ./init_2_add_git_config.sh
 ./init_2_download_git_completion.sh
-[[ $(uname) == "Darwin" ]] && bash -c ./init_2_create_icloud_symlink.sh
+if [[ $(uname) == "Darwin" ]];then
+	bash -c ./init_2_create_icloud_symlink.sh
+	bash -c ./scripts/defaults_write.sh
+fi
 
 ./scripts/forzsh.sh
 ./asdf/_install.sh
