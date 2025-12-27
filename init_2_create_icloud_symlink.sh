741b823dd8ed060571868b7c60530f4883b2b625
t-akira012 <aki003xyz@gmail.com>
2025-12-27 12:25:37 +0900

    update

--- init_2_create_icloud_symlink.sh
+++ init_2_create_icloud_symlink.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eux
 
-ln -si "$HOME/Library/Mobile Documents/com~apple~CloudDocs/" "$HOME/iCloud"
-ln -si "$HOME/Library/Mobile Documents/com~apple~CloudDocs/_Works" "$HOME/Works"
-ln -si "$HOME/Library/Mobile Documents/com~apple~CloudDocs/.ssh" "$HOME/.ssh"
+if [[ ! -e $HOME/iCloud ]];then
+    ln -si "$HOME/Library/Mobile Documents/com~apple~CloudDocs/" "$HOME/iCloud"
+fi
+if [[ ! -e $HOME/Works ]];then
+    ln -si "$HOME/Library/Mobile Documents/com~apple~CloudDocs/_Works" "$HOME/Works"
+fi
+if [[ ! -e $HOME/.ssh ]];then
+    ln -si "$HOME/Library/Mobile Documents/com~apple~CloudDocs/.ssh" "$HOME/.ssh"
+fi
