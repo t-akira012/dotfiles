46a1596f2cada7d7c1517ac29c2db0da96790b9e
t-akira012 <aki003xyz@gmail.com>
2025-12-31 01:27:19 +0900

    update

--- dl.sh
+++ dl.sh
@@ -2,6 +2,13 @@
 set -eu
 cd $(dirname $0)
 
-curl -L "https://github.com/t-akira012/dotfiles/archive/main.tar.gz" | tar zxv
-mv dotfiles-main "$HOME/dotfiles"
+if command -v git > /dev/null 2>&1;then
+    git clone https://github.com/t-akira012/dotfiles
+elif command -v curl > /dev/null 2>&1 ;then
+    curl -L "https://github.com/t-akira012/dotfiles/archive/main.tar.gz" | tar zxv
+    mv dotfiles-main "$HOME/dotfiles"
+else
+    echo 'require git or curl'
+fi
+
 bash -c "$HOME/dotfiles/init.sh"
