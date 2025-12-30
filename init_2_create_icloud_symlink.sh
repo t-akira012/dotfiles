053086d647ee98adffb45af2460ba08890af81a1
t-akira012 <aki003xyz@gmail.com>
2025-12-30 13:16:28 +0900

    update

--- init_2_create_icloud_symlink.sh
+++ init_2_create_icloud_symlink.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -eux
 
+
+if [[ ! -L $HOME/ssd/ ]];then
+    ln -si "/Volumes/WD_BLACK_SN7100_2TB_20251229" "$HOME/ssd"
+fi
+
+if [[ ! -L $HOME/Downloads/PxDownloader/ ]];then
+    ln -si "/Volumes/WD_BLACK_SN7100_2TB_20251229/archives/PxDownloader" "$HOME/Downloads/PxDownloader"
+fi
+
 if [[ ! -L $HOME/iCloud ]];then
     ln -si "$HOME/Library/Mobile Documents/com~apple~CloudDocs/" "$HOME/iCloud"
 fi
+
 if [[ ! -L $HOME/Works ]];then
     ln -si "$HOME/Library/Mobile Documents/com~apple~CloudDocs/_Works" "$HOME/Works"
 fi
