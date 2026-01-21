41e1db6372ffc9cc2cd19dc17625733c7c06fd64
t-akira012 <aki003xyz@gmail.com>
2026-01-21 15:37:13 +0900

    update

--- root/bin/tmux-dev.sh
+++ root/bin/tmux-dev.sh
@@ -8,7 +8,7 @@ tmux has-session -t $SESSION_NAME 2>/dev/null
 if [ $? \!= 0 ]; then
     # セッションが存在しない場合、新規作成
     
-    tmux new-session -d -s $SESSION_NAME -n "f"
+    tmux new-session -d -s $SESSION_NAME -n "finder"
     tmux new-window -t $SESSION_NAME:2 -n "dev"
     tmux new-window -t $SESSION_NAME:3 -n "doc"
     tmux new-window -t $SESSION_NAME:8 -n "env"
