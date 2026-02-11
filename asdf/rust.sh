6d653a8446fd7cb095f294b6652e4d731c7686dd
t-akira012 <aki003xyz@gmail.com>
2026-02-11 13:16:59 +0900

    update

--- asdf/rust.sh
+++ asdf/rust.sh
@@ -6,7 +6,7 @@ echo Install Rust
 #########################################################
 #
 if ! type rustup > /dev/null 2>&1 ;then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
 else
     echo "Rust already installed."
 fi
--- init_3/set-mac-hostname.sh
+++ init_3/set-mac-hostname.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-NAME=m4mini-prv
+read -p "Enter hostname: " NAME
 sudo scutil --set ComputerName $NAME
 sudo scutil --set LocalHostName $NAME
 sudo scutil --set HostName $NAME
