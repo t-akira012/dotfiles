d3060211577449e8c72308071299036df83ab9a0
t-akira012 <aki003xyz@gmail.com>
2026-02-16 17:28:36 +0900

    update

--- win/bin/cmdrun.bat
+++ win/bin/cmdrun.bat
@@ -1,5 +1,6 @@
 @echo off
 chcp 65001
 set PATH=%PATH%;%USERPROFILE%\bin;%USERPROFILE%\exe;%USERPROFILE%\exe\git-bash\bin;%USERPROFILE%\exe\git-bash\usr\bin;%USERPROFILE%\exe\nvim-win64\bin
+set XDG_CONFIG_HOME=%USERPROFILE%\.config
 doskey /macrofile=%USERPROFILE%\bin\init.macros
 cmd.exe
