324f029d52b98cf38a1181c3c51be53c67aab4ca
t-akira012 <aki003xyz@gmail.com>
2026-02-16 14:29:32 +0900

    update

--- win/bin/cmdrun.bat
+++ win/bin/cmdrun.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001
-set PATH=%PATH%;%USERPROFILE%\bin;%USERPROFILE%\exe;%USERPROFILE%\exe\git-bash\bin;%USERPROFILE%\exe\git-bash\usr\bin
+set PATH=%PATH%;%USERPROFILE%\bin;%PATH%;%USERPROFILE%\Downloads\bin;%USERPROFILE%\exe;%USERPROFILE%\exe\git-bash\bin;%USERPROFILE%\exe\git-bash\usr\bin
 cmd.exe
