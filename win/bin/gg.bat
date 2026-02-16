cc15d37ba1cc0b366c4a77ddb3d93a8949b62b8f
t-akira012 <aki003xyz@gmail.com>
2026-02-16 17:57:46 +0900

    update

--- win/bin/gg.bat
+++ win/bin/gg.bat
@@ -1,4 +1,4 @@
 @echo off
-for /f "usebackq delims=" %%i in (`ghq list ^| fzf --preview "type %GOPATH%\ghq\%%i\README.md"`) do (
-    cd /d %GOPATH%\ghq\%%i
+for /f "usebackq delims=" %%i in (`ghq list ^| fzf --preview "type %USERPROFILE%\ghq\%%i\README.md"`) do (
+    cd /d %USERPROFILE%\ghq\%%i
 )
