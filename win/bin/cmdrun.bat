e219a4dd35a9cfdad9fddfbd058bf24a3bd5f266
t-akira012 <aki003xyz@gmail.com>
2026-02-16 14:58:11 +0900

    update

--- win/bin/cmdrun.bat
+++ win/bin/cmdrun.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001
-set PATH=%PATH%;%USERPROFILE%\bin;%PATH%;%USERPROFILE%\exe;%USERPROFILE%\exe\git-bash\bin;%USERPROFILE%\exe\git-bash\usr\bin;%USERPROFILE%\exe\nvim-win64\bin
+set PATH=%PATH%;%USERPROFILE%\bin;%USERPROFILE%\exe;%USERPROFILE%\exe\git-bash\bin;%USERPROFILE%\exe\git-bash\usr\bin;%USERPROFILE%\exe\nvim-win64\bin
 cmd.exe
--- win/bin/rm.bat
+++ win/bin/rm.bat
@@ -1,2 +1,4 @@
 @echo off
-del %*
+for %%i in (%*) do (
+    powershell -NoProfile -Command "Add-Type -AssemblyName Microsoft.VisualBasic; $p='%%~fi'; if(Test-Path $p -PathType Container){[Microsoft.VisualBasic.FileIO.FileSystem]::DeleteDirectory($p,'OnlyErrorDialogs','SendToRecycleBin')}else{[Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile($p,'OnlyErrorDialogs','SendToRecycleBin')}"
+)
