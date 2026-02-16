9b96c884e9245503be640fb7085b8d2c165757af
t-akira012 <aki003xyz@gmail.com>
2026-02-16 20:22:04 +0900

    update

--- win/bin/tf.bat
+++ win/bin/tf.bat
@@ -1,4 +1,4 @@
 @echo off
 set "DEPTH=%~1"
 if "%DEPTH%"=="" set "DEPTH=9"
-for /f "delims=" %%d in ('eza -TDa -L %DEPTH% -I node_modules -I .git ^| fzf') do cd /d "%%d"
+for /f "delims=" %%d in ('eza -Da -R -L %DEPTH% -I node_modules -I .git ^| fzf') do cd /d "%%d"
