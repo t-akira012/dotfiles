7f33c4f52a36aaf27d78dde68d9437f0fbb0e581
t-akira012 <aki003xyz@gmail.com>
2026-03-23 10:43:14 +0900

    update

--- root/bin/func/fzf-tmux-popup-shell.sh
+++ root/bin/func/fzf-tmux-popup-shell.sh
@@ -3,8 +3,12 @@
 popup="$HOME/bin/func/fzf-tmux-popup.sh"
 cmd=$("$popup" --input "Shell: ")
 [[ -z "$cmd" ]] && exit 0
-if ! zsh -c "command -v ${cmd%% *}" >/dev/null 2>&1; then
-  cmd="__fzf-search $cmd"
-fi
+# if ! zsh -ic "command -v ${cmd%% *}" >/dev/null 2>&1; then
+#   cmd="__fzf-search $cmd"
+# fi
+case "${cmd%% *}" in
+  b|s|a) ;;
+  *) cmd="__fzf-search $cmd" ;;
+esac
 zsh -ic "$cmd"
 exit 0
