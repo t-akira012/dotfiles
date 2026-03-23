ad704328af52c2caae1da2ce184adec8b4f9a4f3
t-akira012 <aki003xyz@gmail.com>
2026-03-23 16:31:55 +0900

    update

--- root/bin/func/fzf-common-pj.sh
+++ root/bin/func/fzf-common-pj.sh
@@ -1,6 +1,8 @@
 PROJECT_MD="$HOME/docs/doc/omnipj.md"
 
 __fzf-projects() {
+  [[ -z "$EDITOR" ]] && { echo "EDITOR is not set" >&2; return 1; }
+
   local popup="$HOME/bin/func/fzf-tmux-popup.sh"
   local selected
   selected="$([[ -f "$PROJECT_MD" ]] && grep -n '^#' "$PROJECT_MD" | "$popup")"
@@ -8,7 +10,14 @@ __fzf-projects() {
   [[ -z "$selected" ]] && return
 
   local line_num="${selected%%:*}"
-  "${EDITOR:-vim}" +"$line_num" "$PROJECT_MD"
+  if ! tmux select-window -t :9 2>/dev/null; then
+    tmux new-window -t :9 -n doc
+    tmux send-keys -t :9 "$EDITOR +${line_num} ${PROJECT_MD}" Enter
+  elif [[ "$(tmux list-panes -t :9 -F '#{pane_current_command}')" == "nvim" ]]; then
+    tmux send-keys -t :9 ":tabnew +${line_num} ${PROJECT_MD}" Enter
+  else
+    tmux send-keys -t :9 "$EDITOR +${line_num} ${PROJECT_MD}" Enter
+  fi
 }
 
 alias p='__fzf-projects'
