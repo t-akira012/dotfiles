803e80dbea0ad86c1ffab26f19e9f160190e7aad
t-akira012 <aki003xyz@gmail.com>
2026-03-23 15:41:35 +0900

    update

--- root/bin/func/fzf-biz.sh
+++ root/bin/func/fzf-biz.sh
@@ -1,16 +1,22 @@
 # data sources: output "url\ttitle" lines
 __query-bookmarks() {
-  local file="$HOME/Library/Application Support/Google/Chrome/Default/Bookmarks"
-  [[ -e "$file" ]] && jq -r '.. | select(.type? == "url") | "\(.url)\t\(.name)"' "$file"
+  local chrome_dir="$HOME/Library/Application Support/Google/Chrome"
+  for file in "$chrome_dir"/Profile\ */Bookmarks; do
+    [[ -e "$file" ]] && jq -r '.. | select(.type? == "url") | "\(.url)\t\(.name)"' "$file"
+  done
 }
 
 __query-history() {
-  local db="$HOME/Library/Application Support/Google/Chrome/Default/History"
-  [[ -e "$db" ]] || return 1
+  local chrome_dir="$HOME/Library/Application Support/Google/Chrome"
+  local dbs=("${(@f)$(find "$chrome_dir" -name "History" -path "*/Profile */History" 2>/dev/null)}")
+  [[ ${#dbs[@]} -eq 0 ]] && return 1
   local tmp=$(mktemp)
-  command cp "$db" "$tmp"
-  sqlite3 "$tmp" "SELECT url, title FROM urls ORDER BY last_visit_time DESC LIMIT 5000;" -separator $'\t' \
-    | awk -F'\t' '!seen[$1]++'
+  {
+    for db in "${dbs[@]}"; do
+      command cp "$db" "$tmp"
+      sqlite3 "$tmp" "SELECT url, title FROM urls ORDER BY last_visit_time DESC LIMIT 5000;" -separator $'\t'
+    done
+  } | awk -F'\t' '!seen[$1]++'
   rm -f "$tmp"
 }
 
