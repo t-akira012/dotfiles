c060bec694691429bf92ea9aaab936dcb22cec70
t-akira012 <aki003xyz@gmail.com>
2026-03-24 16:51:20 +0900

    update

--- init_1_homebrew_for_mac.sh
+++ init_1_homebrew_for_mac.sh
@@ -31,7 +31,7 @@ fi
 if [[ $(uname) == "Darwin" ]]; then
 	echo "=== Install Homebrew packages"
 	brew install --quiet \
-		git gh curl wget tree tmux watch expect z unar nvim shfmt bash-completion git-delta yazi zoxide
+		git gh curl wget tree tmux watch expect z unar nvim shfmt bash-completion git-delta yazi zoxide betterleaks
 
 	echo "=== Install GNU core packages for macOS"
 	brew install --quiet \
