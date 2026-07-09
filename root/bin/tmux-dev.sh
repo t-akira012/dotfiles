966d2d662598d7460326d85e726da588a97b280b
t-akira012 <aki003xyz@gmail.com>
2026-07-09 19:38:17 +0900

    update

--- root/bin/tmux-dev.sh
+++ root/bin/tmux-dev.sh
@@ -9,7 +9,7 @@ if [ $? \!= 0 ]; then
     # セッションが存在しない場合、新規作成
     
     tmux new-session -d -s $SESSION_NAME -n "prv"
-    tmux new-session -d -s $SESSION_NAME -n "dev"
+    tmux new-window -t $SESSION_NAME:2 -n "dev" -c "$HOME/dev"
     tmux new-window -t $SESSION_NAME:8 -n "dotfiles" -c "$HOME/dotfiles"
     tmux new-window -t $SESSION_NAME:9 -n "doc" -c $HOME/docs/doc/
     
